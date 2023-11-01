d132e775d6a74f778a053c187aa6f6d8db83b8da
Erwen Li <erwenlovelife@outlook.com>
2023-11-01 13:10:56 +0800

    deploy demo to gh-pages done!

--- vite.gh-pages-config.ts
+++ vite.gh-pages-config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "./dist"
+    outDir: "./dist",
   },
+  base: "/react-json-hook/",
   optimizeDeps: {
     include: ['react', 'react-dom'],
   },
